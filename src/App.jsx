9e5d3e84886fd6cd324b16027d040af49cf0e56e
Tunahan Karakaya <bstunahan@gmail.com>
2024-03-21 16:04:39 +0100

    added tailwindcss to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
-
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello World :)</h1>
+      <h1 className="text-3xl font-bold underline"><img src="" /> To-Do App</h1>
+      <div className="important-task-list">
+        <h2>Important</h2>
+        <ul>
+          <li>Task 1</li>
+          <li>Task 2</li>
+        </ul>
+        <div className="task-list">
+          <h2>Tasks</h2>
+          <ul>
+            <li>Task 3</li>
+            <li>Task 4</li>
+          </ul>
+        </div>
+      </div>
+      <footer className="footer">
+        <input type="text" placeholder="What do you need to do?" /><button>+ Add</button>
+      </footer>
     </>
   )
 }
